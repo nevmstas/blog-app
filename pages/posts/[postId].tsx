2daef6fdf05894b3f48a87df98c86e8a25f241cf
Stas <nevmyvakastas@gmail.com>
2020-09-01 23:45:00 +0600

    fixed getting post by id with getInitialProps

--- pages/posts/[postId].tsx
+++ pages/posts/[postId].tsx
@@ -1,36 +1,21 @@
 import { useRouter } from 'next/router'
+
 export default function Post({ post }){
-        console.log('kekw'+post)
         return (
             <div>
-                {/* <h1>Post {post.title}</h1>
-                <p>{post.body}</p> */}
+                <h1>Post {post.title}</h1>
+                <p>{post.body}</p>
             </div>
                     
         )
         
 }
 
-
-// export async function getStaticProps(ctx) {
-//     const response = await fetch('https://simple-blog-api.crew.red/posts/' +ctx.query.postId);
-//     const post = await response.json();
-//     return {
-//       props: {
-//         post,
-//       }
-//     }
-//   }
-
-//   Post.getInitialProps = async (ctx) => {
-//       debugger
-//     console.log('kek'+ctx)
-//     const response = await fetch('https://simple-blog-api.crew.red/posts/' + ctx.query.postId);
-//     const post = await response.json();
-//     console.log(post)
-//     return {
-//       props: {
-//         post
-//       }
-//     }
-//   }
+Post.getInitialProps = async (ctx) => {
+    const response = await fetch('https://simple-blog-api.crew.red/posts/' + ctx.query.postId);
+    const post = await response.json();
+    console.log(post)
+    return {
+      post
+    }
+}

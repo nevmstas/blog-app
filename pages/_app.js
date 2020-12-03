545ec1d5bbbc83d51e122eda5f559b4cb70fb37c
Stas <nevmyvakastas@gmail.com>
2020-12-03 23:57:42 +0600

    nest init, delete past

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,7 @@
 import '../styles/globals.css'
-import { useStore } from '../redux/store'
-import { Provider } from 'react-redux'
 
 function MyApp({ Component, pageProps }) {
-  const store = useStore(pageProps.initialReduxState)
-
-  return (
-    <Provider store = {store}>
-        <Component {...pageProps} />
-    </Provider>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
